8c85a2faac9243ad5b165da1de74979108dc5395
Tom Mertz & Tommy Bountham <student@epicodus.com>
2015-03-30 13:51:22 -0700

    Add user test file

---
+++ tests/UserTest.php
@@ -0,0 +1,17 @@
+<?php
+
+    require_once 'src/User.php';
+
+    //add DB initialization later
+
+    class UserTest extends PHPUnit_Framework_TestCase
+    {
+        protected function tearDown()
+        {
+
+        }
+    }
+
+
+
+ ?>
