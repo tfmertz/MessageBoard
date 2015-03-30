bf777714b5050aafe9f06b487867b4d6bd48fc6a
chitra atmakuru & nhu finney <student@epicodus.com>
2015-03-30 15:45:48 -0700

    Add message.php

---
+++ src/Message.php
@@ -0,0 +1,17 @@
+<?php
+
+    class Message
+    {
+        private $id;
+        private $text;
+        private $user_id;
+
+        function __construct()
+        {
+            
+        }
+    }
+
+
+
+ ?>
