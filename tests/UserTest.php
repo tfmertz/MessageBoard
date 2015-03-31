c241ebc10c7d406aee15d8a208a7eecbd2a20b78
Virginie Trubiano & Conor Baumgart <student@epicodus.com>
2015-03-31 13:44:32 -0700

    Remove getIsAdmin

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -154,134 +154,11 @@
 
 
             $result = User::getAll();
+
+            $this->assertEquals([$test_user, $test_user2], $result);
         }
 
-        // function test_getIsAdmin()
-        // {
-        //     $name = "Tyler";
-        //     $isadmin = true;
-        //     $password = "howdy";
-        //     $test_user = new User($name, $isadmin, $password);
-        //
-        //     $result = $test_user->getIsAdmin();
-        //
-        //     $this->assertEquals(true, $result);
-        // }
-        //
-        // function test_getPassword()
-        // {
-        //     $name = "Tyler";
-        //     $isadmin = true;
-        //     $password = "howdy";
-        //     $test_user = new User($name, $isadmin, $password);
-        //
-        //     $result = $test_user->getPassword();
-        //
-        //     $this->assertEquals("howdy", $result);
-        // }
-        //
-        // function test_getId()
-        // {
-        //     $name = "Tyler";
-        //     $isadmin = true;
-        //     $password = "howdy";
-        //     $id = 1;
-        //     $test_user = new User($name, $isadmin, $password, $id);
-        //
-        //     $result = $test_user->getId();
-        //
-        //     $this->assertEquals(1, $result);
-        // }
-        //
-        // function test_setName()
-        // {
-        //     $name = "Tyler";
-        //     $isadmin = true;
-        //     $password = "howdy";
-        //     $test_user = new User($name, $isadmin, $password);
-        //     $new_name = "Taylor";
-        //
-        //     $test_user->setName($new_name);
-        //     $result = $test_user->getName();
-        //
-        //     $this->assertEquals("Taylor", $result);
-        //
-        // }
-        //
-        // function test_setIsAdmin()
-        // {
-        //     $name = "Tyler";
-        //     $isadmin = true;
-        //     $password = "howdy";
-        //     $test_user = new User($name, $isadmin, $password);
-        //     $new_isadmin = false;
-        //
-        //     $test_user->setIsAdmin($new_isadmin);
-        //     $result = $test_user->getIsAdmin();
-        //
-        //     $this->assertEquals(false, $result);
-        // }
-        //
-        // function test_setPassword()
-        // {
-        //     $name = "Tyler";
-        //     $isadmin = true;
-        //     $password = "howdy";
-        //     $test_user = new User($name, $isadmin, $password);
-        //     $new_password = "hey there";
-        //
-        //     $test_user->setPassword($new_password);
-        //     $result = $test_user->getPassword();
-        //
-        //     $this->assertEquals("hey there", $result);
-        // }
-        //
-        // function test_setId()
-        // {
-        //     $name = "Tyler";
-        //     $isadmin = true;
-        //     $password = "howdy";
-        //     $test_user = new User($name, $isadmin, $password);
-        //     $new_id = 45;
-        //
-        //     $test_user->setId($new_id);
-        //     $result = $test_user->getId();
-        //
-        //     $this->assertEquals(45, $result);
-        // }
-        //
-        // function test_save()
-        // {
-        //     $name = "Tyler";
-        //     $isadmin = true;
-        //     $password = "howdy";
-        //     $test_user = new User($name, $isadmin, $password);
-        //     $test_user->save();
-        //
-        //     $result = User::getAll();
-        //
-        //     $this->assertEquals([$test_user], $result);
-        // }
-        //
-        // function test_getAll()
-        // {
-        //     $name = "Tyler";
-        //     $isadmin = true;
-        //     $password = "howdy";
-        //     $test_user = new User($name, $isadmin, $password);
-        //     $test_user->save();
-        //
-        //     $name2 = "Richard";
-        //     $password2 = "bouh";
-        //     $test_user2 = new User($name2, $isadmin, $password2);
-        //     $test_user2->save();
-        //
-        //
-        //     $result = User::getAll();
-        //
-        //     $this->assertEquals([$test_user, $test_user2], $result);
-        // }
-
+    
         function test_updatePassword()
         {
             $name = "Tyler";
@@ -296,7 +173,6 @@
 
             $this->assertEquals($new_password, $result);
         }
-
 
         // function test_getMessages()
         // {
@@ -313,19 +189,6 @@
         //
         //     $this->assertEquals([$message], $result);
         // }
-
-        function test_checkAvailable()
-        {
-            $name = "Tyler";
-            $isadmin = true;
-            $password = "howdy";
-            $test_user = new User($name, $password, $isadmin);
-            $new_password = 'rebouh';
-
-            $result= $test_user->checkAvailable($name);
-
-            $this->assertEquals(true, $result);
-        }
 
         function test_delete()
         {
