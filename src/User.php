372c16535c00b17c9eb6eaa81b5a6dc7300c21ac
virginie <virginietrubiano@gmail.com>
2015-03-31 09:47:14 -0700

    change name variable

--- src/User.php
+++ src/User.php
@@ -7,9 +7,9 @@
         private $password;
         private $id;
 
-        function __construct($name, $isAdmin, $password, $id = null)
+        function __construct($new_name, $isAdmin, $password, $id = null)
         {
-            $this->name = $name;
+            $this->name = $new_name;
             $this->isAdmin = $isAdmin;
             $this->password = $password;
             $this->id = $id;
