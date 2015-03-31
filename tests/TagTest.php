b3ef1e6280190d4f7d1bd0f12a21497671b0c957
Tom Mertz & tommy bountham <student@epicodus.com>
2015-03-31 09:38:31 -0700

    Add tag test file

---
+++ tests/TagTest.php
@@ -0,0 +1,17 @@
+<?php
+
+    
+
+    require_once 'src/Tag.php';
+
+
+    class TagTest extends PHPUnit_Framework_TestCase
+    {
+        protected function tearDown()
+        {
+
+        }
+    }
+
+
+ ?>
