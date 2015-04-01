f2488aba980a3f56a5d2ba190036989dbe3a0a52
Tom Mertz <tom.mertz@verizon.net>
2015-04-01 14:10:47 -0700

    Connect the pieces

--- app/app.php
+++ app/app.php
@@ -12,16 +12,24 @@
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/../views'
     ));
+
+    //information for patch/delete requests and silex forward
     use Symfony\Component\HttpFoundation\Request;
+    use Symfony\Component\HttpKernel\HttpKernelInterface;
     Request::enableHttpMethodParameterOverride();
 
 
     //Route to home page
-    $app->get("/", function() use ($app) {
+    $app->get("/", function(Request $request) use ($app) {
+        $error = "false";
 
+        $query = $request->query->all();
 
+        if($query) {
+            $error = $query['error'];
+        }
 
-        return $app['twig']->render('index.twig');
+        return $app['twig']->render('index.twig', array('error' => $error));
     });
 
 
@@ -37,17 +45,17 @@
 
     $app->post("/sign_up", function() use ($app) {
 
-        if (!$_POST['username']){
-            $user_name = $_POST['username'];
-            if (str_word_count($user_name)>1){
-                if (User::checkAvailable($user_name))
-                    {
-                        $password= $_POST['password'];
-                        $new_user = new User($user_name, $password);
-                        $new_user->save();
-                    } else $alert= "this User name is already exit. Please choose another User name";
-                } else $alert= "cannot space two words";
-            } else $alert= "please fill in the user name";
+        // if (!$_POST['username']){
+        //     $user_name = $_POST['username'];
+        //     if (str_word_count($user_name)>1){
+        //         if (User::checkAvailable($user_name))
+        //             {
+        //                 $password= $_POST['password'];
+        //                 $new_user = new User($user_name, $password);
+        //                 $new_user->save();
+        //             } else $alert= "this User name is already exit. Please choose another User name";
+        //         } else $alert= "cannot space two words";
+        //     } else $alert= "please fill in the user name";
 
     return $app['twig']->render('sign_up.twig', array());
     });
@@ -55,13 +63,33 @@
 //********LOGIN****************LOGIN****************LOGIN***********
 
     $app->post("/login", function() use ($app) {
-        $alert="";
-        if (!$_POST['username'] && !$_POST['password'] ){
-            $user_name = $_POST['username'];
-            $password= $_POST['password'];
-        } else { $alert="user name and password do not match, try again or sign up new account";}
 
-    return $app['twig']->render('login.twig', array());
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $user = User::logInCheck($username, $password);
+
+
+
+        if($user) {
+            $subRequest = Request::create('/messages', 'POST', array('user' => $user));
+
+            return $app->handle($subRequest, HttpKernelInterface::SUB_REQUEST);
+        } else {
+            $error = "true";
+            $subRequest = Request::create('/', 'GET', array('error' => $error));
+
+            return $app->handle($subRequest, HttpKernelInterface::SUB_REQUEST, true);
+        }
+
+        return $app["twig"]->render('messages.twig', array('user' => $user));
+
+        // $alert="";
+        // if (!$username && !$_POST['password'] ){
+        //     $user_name = $_POST['username'];
+        //     $password= $_POST['password'];
+        // } else { $alert="user name and password do not match, try again or sign up new account";}
+        //
+        // return $app['twig']->render('login.twig', array('check' => $check));
     });
 
     return $app;
